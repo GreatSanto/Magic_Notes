39fc4b56a0adfd37d08816b7612627a33196069d
GreatSanto <834046+GreatSanto@users.noreply.github.com>
2021-07-29 00:58:18 +0530

    main change

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
   if (addTxt.value.length == 0){
-    alert("Your notes are empty!");
+    show('Failure', 'Please add some text to add.');
   }
   else{
-  notesObj.push(addTxt.value);
+  notesObj.push(myObj);
+  show('success', 'Your note has been successfully added');
 }
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   showNotes();
 });
 
@@ -30,15 +37,16 @@ function showNotes() {
   } else {
     notesObj = JSON.parse(notes);
   }
+
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body" id="card-area">
-                        <h5 class="card-title">${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
-                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                        <button class="btn btn-primary" id="impBtn" onclick="markImportant()" ondblclick="unMarkImportant()"><span id="marking">Mark It</span></button>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary delBtn">Delete Note</button>
+                        <button class="btn btn-primary impBtn" id="impBtn${index}" onclick="markImportant(this.id)" ondblclick="unMarkImportant(this.id)"><span id="marking${index}">Mark It</span></button>
                     </div>
                 </div>`;
   });
@@ -73,7 +81,8 @@ search.addEventListener("input", function(){
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element){
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
+        let cardTit = element.getElementsByTagName("h5")[0].innerText;
+        if(cardTxt.includes(inputVal) || cardTit.includes(inputVal)){
             element.style.display = "block";
         }
         else{
@@ -81,15 +90,26 @@ search.addEventListener("input", function(){
         }
     })
 })
-function markImportant() {
-    var x = document.getElementById("impBtn");
-    var y = document.getElementById("card-area");
-    y.style.setProperty("background-color", "#FF5757", "important");
-    document.getElementById("marking").innerHTML = "UnMark";
+function markImportant(index) {
+    var y = document.getElementById(index);
+    var x = document.getElementById(index).parentElement;
+    x.style.setProperty("background-color", "#FF5757", "important");
+    document.getElementById(index).innerHTML = "UnMark";
 }
-function unMarkImportant() {
-    var x = document.getElementById("impBtn");
-    var y = document.getElementById("card-area");
-    y.style.setProperty("background-color", "white");
-    document.getElementById("marking").innerHTML = "Mark It";
+function unMarkImportant(index) {
+    var y = document.getElementById(index);
+    var x = document.getElementById(index).parentElement;
+    x.style.setProperty("background-color", "#fff");
+    document.getElementById(index).innerHTML = "Mark It";
+}
+
+function show(type, displayMessage) {
+    let message = document.getElementById('message');
+    message.innerHTML = `<span class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Messge:</strong> ${displayMessage}
+                        </span>`;
+    setTimeout(function () {
+        message.innerHTML = ''
+    }, 2000);
+
 }
